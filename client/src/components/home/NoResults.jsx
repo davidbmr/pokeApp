3d9aac52ac396c3e7019897f2d1f7cb7d99aefa9
davidMachuca <davidmachucarodriguez@gmail.com>
2022-04-09 11:57:51 -0500

    agregando contenido al componente noResults y dando pequeños estilos

--- client/src/components/home/NoResults.jsx
+++ client/src/components/home/NoResults.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import style from "./styles/NoResults.module.css";
+import imgNoResults from "../../assets/imgNoResults.jpg";
 
 const NoResults = () => {
 	return (
-		<div>
-			<div>No se pudo encontrar el pokemon</div>
+		<div className={style.noResultsContainer}>
+			<p className={style.noResultsText}>
+				Lo sentimos, no se pudo encontrar el pokemon o la lista de pokemones que
+				estabas buscando
+			</p>
+			<img
+				className={style.noResultsImg}
+				src={imgNoResults}
+				alt='pikachu triste'
+			/>
 		</div>
 	);
 };
